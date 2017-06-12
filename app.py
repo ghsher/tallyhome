2555327deebfb04f186031b7831c09d7baca1424
Gabriel <gabriel.henry.sher@gmail.com>
2017-06-12 19:44:20 -0400

    committing MVP to heroku and github

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from bokeh.plotting import figure, output_file, show
+from bokeh.charts import Donut, Bar, Histogram, show
+from bokeh.embed import components
+from bokeh.resources import CDN
+from bokeh.palettes import viridis
+import pandas as pd
+from arraytype import MutableList
+from numpy import pi
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:postpass123@localhost/tallyhome"
@@ -8,11 +15,11 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 db=SQLAlchemy(app)
 
 class Data(db.Model):
-    __tablename__ = "data"
+    __tablename__ = "tallies"
     id=db.Column(db.Integer, primary_key=True)
     vistype_=db.Column(db.String(30))
     qsandas_=db.Column(db.ARRAY(db.String(120)))
-    results_=db.Column(db.ARRAY(db.Integer))
+    results_=db.Column(MutableList.as_mutable(db.ARRAY(db.Integer)))
 
     def __init__(self, vistype_, qsandas_, results_):
         self.vistype_=vistype_
@@ -37,28 +44,26 @@ def success():
             qsandas = [request.form["xq"],request.form["xaxis"],request.form["xmin"],
             request.form["xmax"],request.form["xstep"],request.form["yq"],
             request.form["yaxis"],request.form["ymin"],request.form["ymax"],request.form["ystep"]]
-            #votes[0] will be an array of x results, votes[1] will be an array of y results
-            results.append([0])
-            results.append([0])
-            #   you will have to ignore results[0][0] and results[1][0],
-            #maybe do the above with each spot in votes being a
-            #   dictionary with an x and y value?
+            # every even index spot in results_[] is an x value ([0],[2],[4]...)
+            # every odd index spot in results_[] is a y value ([1],[3],[5]...)
         elif (vistype == "pie"):
             qsandas.append(request.form["question"])
             qsandas.append(request.form["numans"])
             for i in range(1, int(request.form["numans"])+1):
                 qsandas.append(request.form["ans"+str(i)])
-                results.append(0) #each option is given a vote spot, 0--> #options-1
+            # each spot in results_[] is the index of voted value
+            #   (voted value = qsandas[results[0]+2])
         elif (vistype == "bar"):
             qsandas.append(request.form["question"])
             qsandas.append(request.form["numans"])
             for i in range(1, int(request.form["numans"])+1):
                 qsandas.append(request.form["ans"+str(i)])
-                results.append(0) #each option is given a vote spot, 0--> #options-1
+            # each spot in results_[] is the index of voted value
+            #   (voted value = qsandas[results[0]+2])
         elif (vistype == "histo"):
-            qsandas = [request.form["q"],request.form["min"],request.form["max"],
+            qsandas = [request.form["q"],request.form["label"],request.form["min"],request.form["max"],
             request.form["step"],request.form["ranges"]]
-            #each space in votes[] is an input, so no need to modify it
+            # each spot in results_[] is the inputted value
         data=Data(vistype,qsandas,results)
         db.session.add(data)
         db.session.commit()
@@ -72,34 +77,66 @@ def show_tally(tally_id):
 @app.route('/<int:tally_id>/results', methods=["POST", "GET"])
 def show_tally_results(tally_id):
     df=Data.query.get(tally_id)
-
-    print(df.results_)
     if request.method=='POST':
         if df.vistype_ == "scatter":
-            print(Data.__table__)
-            print(type(df.results_))
-            df.results_[0].append(request.form["x"])
-            df.results_[1].append(request.form["y"])
-            print(df.results_)
-            print(type(df.results_))
+            df.results_.append(int(request.form["x"]))
+            df.results_.append(int(request.form["y"]))
         elif df.vistype_ == "pie":
-            df.results_[int(request.form["pans"])] += 1
+            df.results_.append(int(request.form["pans"]))
         elif df.vistype_ == "bar":
-            df.results_[int(request.form["bans"])] += 1
+            df.results_.append(int(request.form["bans"]))
         elif df.vistype_ == "histo":
-            df.results_.append(request.form["histo_input"])
-        #print(df.results_)
-        #db.session.add(df)
+            df.results_.append(int(request.form["histo_input"]))
+        db.session.commit()
     if df.vistype_ == "scatter":
-        plot=figure(plot_width=500, plot_height=400, title=df.qsandas_[0])
-        plot.xaxis.axis_label=df.qsandas_[1]
-        plot.yaxis.axis_label=df.qsandas_[6]
-        plot.circle(df.results_[0][1:], df.results_[1][1:], size=5, color="red", alpha=0.5)
-        show(plot)
-    print(df.results_)
-    db.session.commit()
-    print(df.results_)
-    return render_template("tally_results.html", id=tally_id)
+        tally=figure(plot_width=500, plot_height=400, title=df.qsandas_[0], tools="")
+        tally.xaxis.axis_label=df.qsandas_[1]
+        tally.yaxis.axis_label=df.qsandas_[6]
+        xvalues=[]
+        yvalues=[]
+        for i in range(0,len(df.results_),2):
+            xvalues.append(df.results_[i])
+            yvalues.append(df.results_[i+1])
+        tally.circle(xvalues, yvalues, size=7, color="red", alpha=0.5)
+    elif df.vistype_ == "pie":
+        numopts = int(df.qsandas_[1])
+        # Create a list of percent vote share for each option in the Tally
+        percents = [df.results_.count(i)/len(df.results_) for i in range(numopts)]
+        # Create a list of colours from the pallete of correct length
+        colours = viridis(numopts)
+        """ # Using plotting - revisit if you can erase axis lines
+            # Would use this because more control over labels, etc.
+        starts = [p*2*pi for p in percents[:-1]]
+        ends = [p*2*pi for p in percents[1:]]
+
+        tally = figure(x_range=(-1,1), y_range=(-1,1))
+        tally.wedge(x=0, y=0, radius=1, start_angle=starts, end_angle=ends, color=colours)
+        show(tally)"""
+        # Create a Pandas series out of percents and answer labels
+        data = pd.Series(percents,
+         index=[df.qsandas_[i+2] for i in range(numopts)])
+        tally = Donut(data, color=colours, hover_text="%",tools="")
+    elif df.vistype_ == "bar":
+        numopts = int(df.qsandas_[1])
+        # Create a list of total vote count for each option in the Tally
+        votecounts = [df.results_.count(i) for i in range(numopts)]
+        # Create a list of colours from the pallete of correct length
+        colours = viridis(numopts)
+        # Create a table out of vote counts and answer labels
+        data = {
+            'option': df.qsandas_[2:],
+            'votes': votecounts
+        }
+        # All one colour, but properly spaced bars:
+        #   tally = Bar(data, values='votes', label='option', color=colours, plot_width=400)
+        tally = Bar(data, values='votes', label='option', color=colours, group='option', plot_width=400,tools="")
+    elif df.vistype_ == "histo":
+        tally = Histogram(df.results_, bins=int(df.qsandas_[5]), plot_width=400,tools="")
+    script1, div1 = components(tally)
+    cdn_js=CDN.js_files[0]
+    cdn_css=CDN.css_files[0]
+    return render_template("tally_results.html", id=tally_id,
+     embedscript=script1, embeddiv=div1, cdncss=cdn_css, cdnjs=cdn_js)
 
 # Run
 if __name__ == '__main__':
